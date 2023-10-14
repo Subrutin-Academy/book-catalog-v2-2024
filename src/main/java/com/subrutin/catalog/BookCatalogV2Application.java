aa146f6b0fb88080b2e2560963499bf3ec1778f9
Tedy Tri Saputro <tedy@saputro.dev>
2023-10-14 16:38:27 +0700

    first commit

---
+++ src/main/java/com/subrutin/catalog/BookCatalogV2Application.java
@@ -0,0 +1,13 @@
+package com.subrutin.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogV2Application.class, args);
+	}
+
+}
