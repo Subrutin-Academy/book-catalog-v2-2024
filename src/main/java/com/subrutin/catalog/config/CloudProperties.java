b2040718f114cea0b157ecb3c8c7829c79a96289
Tedy Tri Saputro <tedy@saputro.dev>
2023-10-14 20:57:55 +0700

    [ADD] add env variable

---
+++ src/main/java/com/subrutin/catalog/config/CloudProperties.java
@@ -0,0 +1,21 @@
+package com.subrutin.catalog.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "cloud")
+public class CloudProperties {
+
+	private String apiKey;
+
+	public String getApiKey() {
+		return apiKey;
+	}
+
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+	
+	
+}
--- src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.subrutin.catalog.config.ApplicationProperties;
+import com.subrutin.catalog.config.CloudProperties;
 import com.subrutin.catalog.service.GreetingService;
 
 @Service
@@ -13,14 +14,18 @@ public class GreetingServiceImpl implements GreetingService {
 
 	private ApplicationProperties appProperties;
 
-	public GreetingServiceImpl(ApplicationProperties appProperties) {
+	private CloudProperties cloudProperties;
+
+	public GreetingServiceImpl(ApplicationProperties appProperties, CloudProperties cloudProperties) {
 		super();
 		this.appProperties = appProperties;
+		this.cloudProperties = cloudProperties;
 	}
 
 	
 	@Override
 	public String sayGreeting() {
+		System.out.println(cloudProperties.getApiKey());
 		TimeZone timezone = TimeZone.getTimeZone(appProperties.getTimezone());
 		return appProperties.getWelcomeText() + ", our timezone :" + timezone.getDisplayName() + ", our currency:"
 				+ appProperties.getCurrency();	
