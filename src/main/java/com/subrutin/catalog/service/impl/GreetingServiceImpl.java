0dc3895c2f817f3020154ac6ed2121fe62b12d80
Tedy Tri Saputro <tedy@saputro.dev>
2023-10-15 00:19:37 +0700

    [ADD] logging

--- src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/subrutin/catalog/service/impl/GreetingServiceImpl.java
@@ -2,6 +2,8 @@ package com.subrutin.catalog.service.impl;
 
 import java.util.TimeZone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,8 @@ import com.subrutin.catalog.service.GreetingService;
 
 @Service
 public class GreetingServiceImpl implements GreetingService {
+
+	Logger log = LoggerFactory.getLogger(GreetingServiceImpl.class);
 
 	private ApplicationProperties appProperties;
 
@@ -25,6 +29,11 @@ public class GreetingServiceImpl implements GreetingService {
 	
 	@Override
 	public String sayGreeting() {
+		log.trace("this is log TRACE");
+		log.debug("this is log DEBUG");
+		log.info("this is log INFO");
+		log.warn("this is log WARN");
+		log.error("this is log ERROR");
 		System.out.println(cloudProperties.getApiKey());
 		TimeZone timezone = TimeZone.getTimeZone(appProperties.getTimezone());
 		return appProperties.getWelcomeText() + ", our timezone :" + timezone.getDisplayName() + ", our currency:"
--- src/main/java/com/subrutin/catalog/web/HelloResources.java
+++ src/main/java/com/subrutin/catalog/web/HelloResources.java
@@ -1,12 +1,17 @@
 package com.subrutin.catalog.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.subrutin.catalog.service.GreetingService;
+import com.subrutin.catalog.service.impl.GreetingServiceImpl;
 
 @RestController
 public class HelloResources {
+
+	Logger log = LoggerFactory.getLogger(HelloResources.class);
 
 	private GreetingService greetingService;
 	
@@ -17,6 +22,12 @@ public class HelloResources {
 
 	@GetMapping("/hello")
 	public String helloWorld() {
+		
+		log.trace("this is log TRACE");
+		log.debug("this is log DEBUG");
+		log.info("this is log INFO");
+		log.warn("this is log WARN");
+		log.error("this is log ERROR");
 		return greetingService.sayGreeting();
 	}
 }
