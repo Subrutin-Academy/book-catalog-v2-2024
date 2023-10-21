33898189e562b2326d771a8e439a0dd22b809dd7
Tedy Tri Saputro <tedy@saputro.dev>
2023-10-21 12:49:08 +0700

    [ADD] add response

--- src/main/java/com/subrutin/catalog/web/HelloResources.java
+++ src/main/java/com/subrutin/catalog/web/HelloResources.java
@@ -3,6 +3,7 @@ package com.subrutin.catalog.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import com.subrutin.catalog.service.GreetingService;
 
 @RestController
 public class HelloResources {
-
+	
 	Logger log = LoggerFactory.getLogger(HelloResources.class);
 
 	private GreetingService greetingService;
@@ -23,9 +24,10 @@ public class HelloResources {
 
 
 
-
+	//get adalah salah satu jenis dari http method/ http verb
+	//POST, PUT, DELETE, OPTION, TRACE, HEAD, PATCH
 	@GetMapping("/hello")
-	public HelloMessageResponseDTO helloWorld() {
+	public ResponseEntity<HelloMessageResponseDTO> helloWorld() {
 		log.trace("this is log TRACE");
 		log.debug("this is log DEBUG");
 		log.info("this is log INFO");
@@ -33,6 +35,6 @@ public class HelloResources {
 		log.error("this is log ERROR");
 		HelloMessageResponseDTO dto = new HelloMessageResponseDTO();
 		dto.setMessage(greetingService.sayGreeting());
-		return dto;
+		return ResponseEntity.accepted().body(dto);
 	}
 }
