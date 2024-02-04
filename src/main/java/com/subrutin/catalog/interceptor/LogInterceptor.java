d78832d585ab9f861a549fb002d06794c66b3fe2
Tedy Tri Saputro <tedy@saputro.dev>
2024-02-04 23:55:31 +0700

    [ADD] add log interceptor

---
+++ src/main/java/com/subrutin/catalog/interceptor/LogInterceptor.java
@@ -0,0 +1,34 @@
+package com.subrutin.catalog.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		log.info("prehandle log interceptor");
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		log.info("posthandle log interceptor");
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		log.info("aftercompletion log interceptor");
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+
+}
--- src/main/java/com/subrutin/catalog/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/subrutin/catalog/repository/impl/BookRepositoryImpl.java
@@ -1,12 +1,5 @@
 package com.subrutin.catalog.repository.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import com.subrutin.catalog.domain.Book;
-import com.subrutin.catalog.repository.BookRepository;
-
 import lombok.Data;
 
 @Data
