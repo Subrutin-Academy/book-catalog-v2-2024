a85d6bcdc2a6c8f23169e1118afafc3472746cb7
Tedy Tri Saputro <tedy@saputro.dev>
2024-02-04 13:59:16 +0700

    Basic Auth membuat Entity Untuk Menyimpan User

---
+++ src/main/java/com/subrutin/catalog/domain/AppUser.java
@@ -0,0 +1,69 @@
+package com.subrutin.catalog.domain;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "app_user", indexes = { @Index(name = "uk_username", columnList = "username") })
+public class AppUser extends AbstractBaseEntity implements UserDetails {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3423593371835697230L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "username", nullable = false)
+	private String username;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
