01249dc9cc002c164b7b04ba2db3fd94e71cb076
Tedy Tri Saputro <tedy@saputro.dev>
2024-02-08 19:23:30 +0700

    define handler bean

--- src/main/java/com/subrutin/catalog/config/AppConfig.java
+++ src/main/java/com/subrutin/catalog/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.subrutin.catalog.domain.Author;
 import com.subrutin.catalog.domain.Book;
 import com.subrutin.catalog.repository.BookRepository;
@@ -20,6 +21,11 @@ public class AppConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
 	}
  
 }
--- src/main/java/com/subrutin/catalog/config/SecurityConfig.java
+++ src/main/java/com/subrutin/catalog/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.subrutin.catalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.subrutin.catalog.security.handler.UsernamePasswordAuthFailureHandler;
+import com.subrutin.catalog.security.handler.UsernamePasswordAuthSucessHandler;
 import com.subrutin.catalog.service.AppUserService;
 
 @EnableWebSecurity
@@ -16,6 +22,16 @@ public class SecurityConfig {
 	
 	@Autowired
 	private AppUserService appUserService;
+	
+	@Bean
+	public AuthenticationSuccessHandler usernamePasswordAuthSuccessHandler(ObjectMapper objectMapper) {
+		return new UsernamePasswordAuthSucessHandler(objectMapper);
+	}
+	
+	@Bean
+	public AuthenticationFailureHandler usernamePasswordAuthFailureHandler(ObjectMapper objectMapper) {
+		return new UsernamePasswordAuthFailureHandler(objectMapper);
+	}
 	
 	@Autowired
 	void registerProvider(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
